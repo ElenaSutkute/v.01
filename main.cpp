41ba8ee20b8fae9c4d54c29695b39ea1e3074f4a
ElenaSutkute <145843117+ElenaSutkute@users.noreply.github.com>
2023-09-30 15:00:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <iomanip>
-using namespace std;
+#include "my_lib.h""
 
-struct Duomenys {
-  char vardas[15];
-  char pavarde[15];
-  vector<int> nd;
-  int egz;
-  double vid;
-  double med;
-};
-int main() {
-  Duomenys g[20];
-  int zmones;
-  int pazimys;
-  cout<<"Iveskite zmoniu skaiciu: ";
-  cin>>zmones;
-  for(int i=0; i<zmones; i++){
-    cout<<"Iveskite varda: ";
-    cin>>g[i].vardas;
-    cout<<"Iveskite pavarde: ";
-    cin>>g[i].pavarde;
-    cout<<"Iveskite namu darbu pazymius noredami ubaigti ivedima, iveskite -1, maksimalus nd kiekis yra 10"<<endl;
-    while (cin>>pazimys){
-    if (pazimys==-1){
-      break;
-    }
-    g[i].nd.push_back(pazimys);
-  }
-  cout<<"Iveskite egzamino rezulata: ";
-  cin>>g[i].egz;
-  }
-  cout<<setw(15)<<left<<"Vardas"<<setw(15)<<left<<"Pavarde";
-  for(int i=1; i<=10; i++){
-    cout<<"Nd"<<i<<" ";
-  }
-  cout<<"Egz"<<endl;
+int main()
+{
+    string ats;
+    vector<Duomenys> studentai;
 
-  for(int i=0; i<zmones; i++){
-    cout<<setw(15)<<left<<g[i].pavarde<<setw(15)<<left<<g[i].vardas;
-    for (int j=0; j<10; j++){
-        if (j<g[i].nd.size()){
-            cout<<setw(4)<<left<<g[i].nd[j];
-        }else{
-        cout<<"    ";
+    cout << "Ar skaityti duomenis is failo? taip/ne" << endl;
+    string tekstinis;
+    try
+    {
+        cin >> tekstinis;
+        if (tekstinis != "taip" && tekstinis != "ne")
+        {
+            throw runtime_error("Error. Veskite tik 'taip' arba 'ne'");
         }
     }
-    cout<<" "<<g[i].egz<<endl;
-  }
+    catch (const runtime_error& e)
+    {
+        cerr << e.what() << endl;
+        exit(EXIT_FAILURE);
+    }
 
+    if (tekstinis == "taip")
+    {
+        studentai = skaitytiDuomenisIsFailo();
+    }
+    else
+    {
+            studentai = ivestiDuomenisRanka();
+    }
 
-  return 0;
+    spausdintiDuomenis(studentai);
+
+    return 0;
 }
+
